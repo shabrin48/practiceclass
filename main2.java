85180f1eaa57d307f28df3ec41fa6b3d03e99b03
shabrin48 <krizzzmat@gmail.com>
2026-01-23 10:57:27 +0200

    commit3

--- main2.java
+++ main2.java
@@ -10,95 +10,106 @@ public class main2 {
     public static void main(String[] args) throws InterruptedException, IOException {
         Scanner sc = new Scanner(System.in);
         Random rand = new Random();
-        int rounds;
-        int time;
-        int score = 0;
-        String user_letters;
-        int user_count_letters;
+        boolean running = true;
 
-        System.out.println("===ГРА ЗАПОМ'ЯТАЙ ЛIТЕРИ У ПРАВЕЛЬНIЙ ПОСЛIДОВНОСТI===");
+        while (running) {
+            int rounds = 0;
+            int time = 0;
+            int score = 0;
+            int user_count_letters = 0;
+            boolean start_game = false;
 
-        System.out.println("--=Головне меню=--");
-        System.out.println("1. Обрати рівень складності");
-        System.out.println("2. Обрати свої налаштуння");
-        int setting_score = sc.nextInt();
+            System.out.println("\n=== ГРА ЗАПОМ'ЯТАЙ ЛIТЕРИ У ПРАВИЛЬНІЙ ПОСЛIДОВНОСТI ===");
+            System.out.println("--= Головне меню =--");
+            System.out.println("1. Обрати рiвень складностi");
+            System.out.println("2. Обрати свої налаштування");
+            System.out.println("3. Вийти з гри");
 
-        switch (setting_score) {
-            case 1:
-        }
+            int choice = sc.nextInt();
+            sc.nextLine();
 
-        System.out.println("Оберiть кiлькiсть раундiв\n3, 4 або 5 раундiв: ");
-        int user_rounds = sc.nextInt();
-        switch (user_rounds) {
-            case 3:
-                rounds = 3;
-                break;
-            case 4:
-                rounds = 4;
-                break;
-            case 5:
-                rounds = 5;
-                break;
-            default:
-                System.out.println("Невiрне обрана кiлькiсть раундiв\nУставленна кiлькiсть раундiв 3");
-                rounds = 3;
-        }
+            switch (choice) {
+                case 1:
+                    new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+                    System.out.println("---Оберiть рiвень складностi---");
+                    System.out.println("1. Легка (2 раунди, 5 сек, 3 літери)");
+                    System.out.println("2. Середня (4 раунди, 3 сек, 5 літер)");
+                    System.out.println("3. Для кiтайцив (5 раундів, 1 сек, 6 літер)");
+                    int difficulty = sc.nextInt();
+                    sc.nextLine();
 
-        System.out.println("Виберiть час для запом'ятовування\n2, 3 або 5 секунд: ");
-        int user_time = sc.nextInt();
-        sc.nextLine();
-        switch (user_time) {
-            case 2:
-                time = 2000;
-                break;
-            case 3:
-                time = 3000;
-                break;
-            case 5:
-                time = 5000;
-                break;
-            default:
-                time = 3000;
-                System.out.println("Невiрний обраний час!!!\nУстановленний час: 3 секунди");
-        }
+                    if (difficulty == 1) {
+                        rounds = 3; time = 5000; user_count_letters = 4;
+                    } else if (difficulty == 2) {
+                        rounds = 4; time = 3000; user_count_letters = 5;
+                    } else {
+                        rounds = 5; time = 1500; user_count_letters = 6;
+                    }
+                    start_game = true;
+                    break;
 
-        System.out.println("Оберiть кiлькiсть лiтер");
-        user_count_letters = sc.nextInt();
-        char[] secret_letters = new char[user_count_letters];
-        sc.nextLine();
+                case 2:
+                    System.out.println("Оберiть кiлькiсть раундiв (3, 4 або 5): ");
+                    rounds = sc.nextInt();
+                    System.out.println("Виберiть час (2, 3 або 5 секунд): ");
+                    int user_time = sc.nextInt();
+                    time = user_time * 1000;
+                    System.out.println("Оберiть кiлькiсть лiтер: ");
+                    user_count_letters = sc.nextInt();
+                    sc.nextLine();
+                    start_game = true;
+                    break;
 
-        System.out.println("Гра розпочалась😈😈😈!!!");
+                case 3:
+                    running = false;
+                    System.out.println("Бувай!");
+                    break;
 
-        for (int i = 1; i <= rounds; i++) {
-            System.out.println("\nРаунд " + i);
-            System.out.println("Запом'ятайте лiтери!!!");
-            for (int j = 0; j < user_count_letters; j++) {
-                char letter = alphabet.charAt(rand.nextInt(alphabet.length()));
-                System.out.print(letter + " ");
-                secret_letters[j] = letter;
+                default:
+                    System.out.println("Невiрний вибiр!");
             }
 
-            Thread.sleep(time); // задержка
-            // очистка
-            new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            if (start_game) {
+                char[] secret_letters = new char[user_count_letters];
 
-            System.out.println("\nВведiть лiтерi у правельнiй послiдовностi: ");
-            user_letters = sc.nextLine();
-            String secret = new String(secret_letters).trim();
+                System.out.println("Гра розпочалась 😈😈😈!!!");
+                Thread.sleep(1000);
 
-            if (user_letters.equals(secret)) {
-                score++;
-                System.out.printf("\nВiрно, у вас %d балiв", score);
-            } else {
-                System.out.printf("\nНе вiрно!!! У вас %d балiв", score);
+                for (int i = 1; i <= rounds; i++) {
+                    System.out.println("\nРаунд " + i);
+                    System.out.println("Запам'ятайте лiтери!!!");
+
+                    for (int j = 0; j < user_count_letters; j++) {
+                        char letter = alphabet.charAt(rand.nextInt(alphabet.length()));
+                        System.out.print(letter + " ");
+                        secret_letters[j] = letter;
+                    }
+                    System.out.println();
+
+                    Thread.sleep(time);
+
+                    new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+
+                    System.out.println("Введiть лiтери у правильнiй послiдовностi: ");
+                    String user_input = sc.nextLine();
+                    String secret = new String(secret_letters);
+
+                    if (user_input.equals(secret)) {
+                        score++;
+                        System.out.printf("Вiрно, у вас %d балiв\n", score);
+                    } else {
+                        System.out.print("\007");
+                        System.out.printf("Не вiрно! Було: %s. У вас %d балiв\n", secret, score);
+                    }
+                }
+
+                System.out.println("\n=== Пiдсумок ===");
+                if (score >= rounds - 1) {
+                    System.out.printf("Вiтаю, ви перемогли! Балiв: %d/%d\n", score, rounds);
+                } else {
+                    System.out.printf("Нажаль, ви програли. Балiв: %d/%d\n", score, rounds);
+                }
             }
-        }
-
-        System.out.println("\n===Пiдсумок===");
-        if (score >= rounds - 1) {
-            System.out.printf("Вiтаю, ви перемогли!!!\nУ вас %d балiв!!!", score);
-        } else {
-            System.out.printf("Нажаль, ви програли((\nУ вас %d балiв\nСпробуйте ще раз😘", score);
         }
         sc.close();
     }
